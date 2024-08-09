93a5662d147013573d754af11e95f2c1376e3e67
Matthew Trent <matthew.rl.trent@gmail.com>
2024-08-09 04:24:47 -0700

    wip

--- impl/1_base.c
+++ impl/1_base.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main() {
-    double M = 0.6;  
+    double M = 891759087;  
 
     printf("base log2(%f) = %f\n", M, log2(M));
 
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#! configure these vars yourself
+#! CONFIG THESE VARS
 DEST_DIR="/home/matthew/remote_computing_convergence_method"
 PI_USER="matthew"
 PI_HOST="192.168.1.124"
 SSH_KEY="~/.ssh/id_rsa"
-#! configure these vars yourself
+#! CONFIG THESE VARS
 
 clear
 
@@ -24,7 +24,9 @@ PI_PATH="$DEST_DIR/$FILE_DIR/$FILE_NAME"
 ssh -i $SSH_KEY $PI_USER@$PI_HOST "mkdir -p $DEST_DIR/$FILE_DIR"
 scp -i $SSH_KEY -r $SOURCE_DIR/* $PI_USER@$PI_HOST:$DEST_DIR
 ssh -i $SSH_KEY $PI_USER@$PI_HOST "ls -lR $DEST_DIR"
+
 OUT_FILE="${PI_PATH%.c}.out"
+
 ssh -i $SSH_KEY $PI_USER@$PI_HOST "gcc $PI_PATH -o $OUT_FILE -lm"
 
 COMPILATION_STATUS=$?
@@ -33,5 +35,19 @@ if [ $COMPILATION_STATUS -ne 0 ]; then
     exit 1
 fi
 
+# run compiled program with perf for report
+REPORT_FILE="${PI_PATH%.c}_perf_report.txt"
+ssh -i $SSH_KEY $PI_USER@$PI_HOST "perf stat -o $REPORT_FILE $OUT_FILE"
+
+# gen report and scp back to local machine
+if ssh -i $SSH_KEY $PI_USER@$PI_HOST "[ -f $REPORT_FILE ]"; then
+    scp -i $SSH_KEY $PI_USER@$PI_HOST:$REPORT_FILE .
+    echo "Performance report:"
+    cat $(basename $REPORT_FILE)
+else
+    echo "Performance report file was not generated or could not be found."
+fi
+
 clear
+
 ssh -t -i $SSH_KEY $PI_USER@$PI_HOST "$OUT_FILE"
